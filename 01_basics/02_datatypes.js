eae00ef7abc889f3247c9c4c59ae2c16634e0afa
Utkarsh3904 <162132082+Utkarsh3904@users.noreply.github.com>
2024-11-08 19:51:45 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //for treating newer verion of js
+
+//alert(3+3) not work as we r using node js, not a browser
+
+//DATATYPES
+
+console.log(typeof "hh")  //string
+
+console.log(typeof null) //object
+
+console.log(typeof undefined)  //undefined
